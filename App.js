d356ceb396c87d3d59190c299ece29585f76446d
Santhosh Umapathi <Santhosh.professional1@gmail.com>
2020-03-31 12:04:16 +0200

    Logic Added for app

--- App.js
+++ App.js
@@ -1,26 +1,85 @@
 import React, { Component } from "react";
 import { StyleSheet, Text, View, TextInput, TouchableOpacity, Dimensions } from "react-native";
-import { Button } from 'native-base';
+import { Button } from "native-base";
 import { FontAwesome } from '@expo/vector-icons';
 
 var itemArray = new Array(25).fill('empty');
 
 export default class ScratchWin extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      text: ""
-    };
-  }
+    constructor(props) {
+      super(props);
+      this.state = {
+        randomNumber: ""
+      };
+    }
+  
+  componentDidMount()
+  {
+    this.setState({randomNumber: this.generateRandomNumber()})
+  };
 
-  render() {
-    return (
-      <View style={styles.container}>
-        <Text>App</Text>
-      </View>
-    );
+  generateRandomNumber = () =>
+  {
+    let randomNumber = Math.floor(Math.random() * 25);
+    this.setState({ randomNumber, isScratched: true});
+    return randomNumber;
+  };
+
+  scratchItem = (itemNumber) => {
+    if (this.state.randomNumber == itemNumber)
+    {
+      itemArray[itemArray] == 'Lucky';
+    }
+    else
+    {
+      itemArray[itemArray] == "Unlucky";
+    }
+    this.forceUpdate;
+  };
+
+  itemIcon = (itemNumber) => {
+    if (itemArray[itemNumber] == 'Lucky')
+    {
+      return 'dollar'
+    }
+    else if (itemArray[itemNumber] == 'Unlucky')
+    {
+      return 'frown-o'
+    }
+    else
+      return 'circle'
+  };
+
+  itemColor = (itemNumber) => { 
+    if (itemArray[itemNumber] == "Lucky") {
+      return "green";
+    } else if (itemArray[itemNumber] == "Unlucky") {
+      return "red";
+    } else return "gray";
+  };
+  
+  showAllItem = () => {
+    itemArray.fill('Unlucky');
+    itemArray[this.state.randomNumber].fill('Lucky');
+    this.forceUpdate();
+  };
+
+  resetGame = () => {
+    this.setState({ randomNumber: this.generateRandomNumber() }, () =>
+    {
+      itemArray.fill('empty');
+      this.forceUpdate;
+    })
+   };
+
+    render() {
+      return (
+        <View style={styles.container}>
+          <Text>App</Text>
+        </View>
+      );
+    }
   }
-}
 
 const styles = StyleSheet.create({
   container: {
